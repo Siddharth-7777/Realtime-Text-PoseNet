895baf755cc1a7fbea5e8b7fa35bee8e702a5e3b
Siddharth-7777 <77883380+Siddharth-7777@users.noreply.github.com>
2021-08-23 18:14:29 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
     canvas = createCanvas(550, 500);
-    canvas.position(560,90)
+    canvas.position(560,110)
     poseNet = ml5.poseNet(video, cheat)
     poseNet.on('pose', gotPoses)
 }
@@ -17,12 +17,21 @@ function cheat() {
     console.log("Cheat Operation is Successful! NOW we have to win the cheating competition.")
 }
 
-function gotPoses(error, results) {
-    if (error) {
-       console.log(error)
-    } else {
-       if (results.length > 0) {
-           console.log(results)
-       }
-    }
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results)
+        nosex = results[0].pose.nose.x
+        nosey = results[0].pose.nose.y
+        lwx = results[0].pose.leftWrist.x
+        rwx = results[0].pose.rightWrist.x
+        dif = Math.abs(rwx - lwx)
+    }  
+}
+
+function draw() {
+    background('blue')
+    textSize(dif)
+    fill('yellow')
+    text('Siddharth', nosex, nosey)  
+    document.getElementById("square_side").innerHTML = "Size of name: " + dif + "px"
 }
